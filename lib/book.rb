4c6b2a1070d4e4637f5a13e6494e71f02bccb899
Regina Furness <skritschy@gmail.com>
2020-04-19 20:03:51 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,26 @@
 class Book 
-  def initialize(title)
+  def initialize(title, author, page_count, genre)
     @title = title
     def title 
       @title
+    end 
+    def author=(author)
+      @author = author
+    end
+    def author
+      @author
+    end
+    def page_count=(num)
+      @page_count = num 
+    end 
+    def page_count
+      @page_count
+    end
+    def genre=(genre)
+      @genre = genre
+    end 
+    def genre
+      @genre
     end 
   end 
 end 
